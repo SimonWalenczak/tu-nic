71a38e12f654124c461c58066e5c642c6390f31c
Etienne Javelle <83096541+Omadel@users.noreply.github.com>
2022-09-09 16:06:42 +0200

    Tuto Menu

--- Assets/_Tunic/Scripts/Runtime/UI/TutoMenu.cs
+++ Assets/_Tunic/Scripts/Runtime/UI/TutoMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using System.Linq;
 
 namespace Tunic
 {
@@ -11,6 +12,7 @@ namespace Tunic
 	    [SerializeField] Button returnButton,nextPageButton,previousPageButton;
 	    
 	    GameObject[] pages;
+	    GameObject currentPageGO;
 	    int maxPages;
 	    int currentPage;
 	    protected void Awake()
@@ -18,7 +20,7 @@ namespace Tunic
 	    	returnButton.onClick.AddListener(Return);
 	    	nextPageButton.onClick.AddListener(NextPage);
 	    	previousPageButton.onClick.AddListener(PreviousPage);
-	    	maxPages=transform.childCount-3;
+	    	maxPages=transform.childCount-4;
 	    	var children = new List<GameObject>();
 	    	foreach (Transform child in transform)
 	    	{
@@ -26,6 +28,7 @@ namespace Tunic
 	    		if (child.gameObject==nextPageButton.gameObject)continue;
 	    		if (child.gameObject==previousPageButton.gameObject)continue;
 	    		children.Add(child.gameObject	);
+	    		child.gameObject.SetActive(false);
 	    	}
 	    	pages= children.ToArray();
 	    	UpdateButtons();
@@ -33,6 +36,7 @@ namespace Tunic
 	    
 	    void Return(){
 	    	gameObject.SetActive(false);
+	    	transform.parent.GetChild(1).gameObject.SetActive(true);
 	    }
 	    
 	    void NextPage(){
@@ -46,8 +50,12 @@ namespace Tunic
 	    }
 	    
 	    void UpdateButtons(){
-		    nextPageButton.gameObject.SetActive(currentPage>=maxPages);
+		    if(currentPageGO!=null)currentPageGO.SetActive(false);
+	    	currentPageGO = pages[currentPage];
+		    currentPageGO.SetActive(true	);
+		    nextPageButton.gameObject.SetActive(currentPage<maxPages);
 		    previousPageButton.gameObject.SetActive(currentPage>0);
+		    
 	    }
     }
 }
