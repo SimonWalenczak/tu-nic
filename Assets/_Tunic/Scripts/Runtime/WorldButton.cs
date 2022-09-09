208167e13f98a369a5a562c516baf5575ba02ec0
Etienne Javelle <83096541+Omadel@users.noreply.github.com>
2022-09-09 14:30:07 +0200

    Talk Buttons

---
+++ Assets/_Tunic/Scripts/Runtime/WorldButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Tunic
+{
+    public class WorldButton : MonoBehaviour, ISelectable
+	{
+		[SerializeField] Transform button;
+		[SerializeField] float pressDepth=.03f;
+		
+		Fadeable fadeable;
+		protected void Awake()
+		{
+			fadeable = GetComponentInChildren<Fadeable>();
+		}
+
+		void ISelectable.Select()
+		{
+			fadeable.FadeIn();
+		}
+
+		void ISelectable.UnSelect()
+		{
+			fadeable.FadeOut();
+		}
+		
+		public void Press(){
+			button	.DOComplete();
+			button.DOMoveY(button.position.y-pressDepth,.2f).SetLoops(2,LoopType.Yoyo);
+		}
+    }
+}
