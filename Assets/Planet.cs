3b2375626409a7a44a5af9c28bcb88fc266c976f
Geoffrey <geoffreey.bernard@gmail.com>
2022-09-09 14:44:41 +0200

    Change Planet With Cursors

--- Assets/Planet.cs
+++ Assets/Planet.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using DG.Tweening;
 
 namespace Tunic
 {
@@ -17,49 +19,84 @@ namespace Tunic
         private Terrain[] terrains;
 
         [SerializeField]
-        private Gradient[] gradients;
+        private Gradient gradient;
+
+        [System.Serializable]
+        class Cursor
+        {
+            public string name;
+            public Color color;
+            public int seaLevel;
+            public GameObject[] props;
+            public float score;
+        }
+
+        [SerializeField]
+        private Cursor[] cursors;
 
         [SerializeField]
         private Transform water;
-
-        bool dirty = false;
 
         private void Start()
         {
             for (int i = 0; i < terrains.Length; ++i)
             {
                 terrains[i].Generate(size);
-                //terrains[i].GenerateProps(0.05f, seaLevel);
+                terrains[i].GenerateProps(0.002f, 5);
             }
+
+            StartCoroutine(ApplyCursors());
         }
 
-        public void applyGradients()
+        private IEnumerator ApplyCursors()
         {
+            float seaLevel = 0;
+
+            float r = 0f;
+            float g = 0f;
+            float b = 0f;
+
+            float totalScore = 0f;
+
+            foreach (Cursor cursor in cursors)
+            {
+                totalScore += cursor.score;
+
+                r += cursor.color.r * cursor.score;
+                g += cursor.color.g * cursor.score;
+                b += cursor.color.b * cursor.score;
+
+                seaLevel += cursor.seaLevel * cursor.score;
+            }
+
+            seaLevel = Mathf.Round(seaLevel / totalScore);
+
+            r /= totalScore;
+            g /= totalScore;
+            b /= totalScore;
+
+            GradientColorKey[] keys = gradient.colorKeys;
+
+            keys[1].color = new Color(r, g, b);
+
+            gradient.SetKeys(keys, gradient.alphaKeys);
+
             for (int i = 0; i < terrains.Length; ++i)
-                terrains[i].ApplyGradient(gradients[i]);
-        }
+                terrains[i].ApplyGradient(gradient);
 
-        private void setSeaLevel(int level)
-        {
-            water.localScale = level > 0 ? Vector3.one * (size + level + 0.1f) : Vector3.zero;
+            water.localScale = seaLevel > 0 ? Vector3.one * (size + seaLevel + 0.1f) : Vector3.zero;
+
+            yield return new WaitForEndOfFrame();
         }
 
         private void Update()
         {
-            if (dirty)
-            {
-                applyGradients();
-                setSeaLevel(seaLevel);
-
-                dirty = false;
-            }
-
             transform.rotation *= Quaternion.Euler(2.5f * rotationSpeed * Time.deltaTime, 5f * rotationSpeed * Time.deltaTime, 0);
         }
 
         private void OnValidate()
         {
-            dirty = true;
+            StartCoroutine(ApplyCursors());
         }
     }
 }
